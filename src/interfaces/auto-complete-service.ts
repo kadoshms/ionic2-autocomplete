f55ed4abba7793dc93db04868954c8b60ac6c30d
mor <kadoshms@gmail.com>
2016-08-18 18:08:31 +0300

    lint fixes in service-interface

--- src/interfaces/auto-complete-service.ts
+++ src/interfaces/auto-complete-service.ts
@@ -3,18 +3,18 @@ export interface AutoCompleteService {
   /**
    * the literal name of the title attribute
    */
-  labelAttribute : string;
+  labelAttribute: string;
 
   /**
    * this method should return an array of objects (results)
    * @param term
    */
-  getResults(term : any) : any;
+  getResults(term: any): any;
 
   /**
    * this method parses each item of the results from data service.
    * the returned value is the displayed form of the result
    * @param item
    */
-  parseItem?(item : any)   : any;
+  parseItem?(item: any): any;
 }
