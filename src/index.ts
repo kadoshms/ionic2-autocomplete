9abbc228a57fbd2ed846720dc18998a30f5836f0
mor <kadoshms@gmail.com>
2016-10-11 20:47:33 +0300

    changed file to suit the new specs

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,6 @@
-import {AUTOCOMPLETE_DIRECTIVES} from './directives';
-import {AUTOCOMPLETE_PIPES} from './pipes';
-
-export * from './directives';
-export * from './pipes';
-
-export {AutoCompleteService} from './interfaces/auto-complete-service';
-
-export default {
-  directives: [AUTOCOMPLETE_DIRECTIVES],
-  pipes: [AUTOCOMPLETE_PIPES]
-}
+/**
+ * Necessary exports
+ */
+export { AUTOCOMPLETE_DIRECTIVES } from './directives';
+export { AUTOCOMPLETE_PIPES } from './pipes';
+export { AutoCompleteService } from './interfaces/auto-complete-service';
