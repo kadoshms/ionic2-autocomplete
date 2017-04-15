0afb14df05cdbe1804e56dea2482fb589e057542
mor <kadoshms@gmail.com>
2017-04-15 20:18:47 +0300

    deleted old files

--- src/index.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Necessary exports
- */
-export { AUTOCOMPLETE_DIRECTIVES } from './directives';
-export { AUTOCOMPLETE_PIPES } from './pipes';
-export { AutoCompleteService } from './interfaces/auto-complete-service';
