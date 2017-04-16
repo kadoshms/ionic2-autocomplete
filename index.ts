e4a211e7ae53273efe3cf24f8845778d4ad583a1
mor <kadoshms@gmail.com>
2017-04-16 17:31:27 +0300

    export the service interface

--- index.ts
+++ index.ts
@@ -9,6 +9,7 @@ import {AutoCompleteItemComponent} from './src/autocompleteitem.component';
 export * from './src/autocomplete.component';
 export * from './src/boldprefix.pipe';
 export * from './src/autocompleteitem.component';
+export * from './src/auto-complete.service';
 
 @NgModule({
     imports: [
