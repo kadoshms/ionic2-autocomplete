48e38ee8d77e7913cd7285f4b54fc4a02001f4b5
callym <hi@callym.com>
2017-05-30 17:15:39 +0100

    Fixes kadoshms/ionic2-autocomplete#45

--- src/boldprefix.pipe.ts
+++ src/boldprefix.pipe.ts
@@ -9,6 +9,7 @@ import { Injectable, PipeTransform, Pipe } from '@angular/core';
 @Injectable()
 export class BoldPrefix implements PipeTransform {
     transform(value: string, keyword: string): any {
-        return value.replace(new RegExp(keyword, 'gi'), function(str) { return str.bold(); });
+        let escaped_keyword = keyword.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+        return value.replace(new RegExp(escaped_keyword, 'gi'), function(str) { return str.bold(); });
     }
 }
