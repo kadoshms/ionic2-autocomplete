cf11cc01c8d3590df0b3f2a7e625de0cb60c8d9d
Jeremy Quick <jrquick@uab.edu>
2019-08-28 13:08:15 -0500

    Make consistent

--- src/auto-complete/auto-complete.component.ts
+++ src/auto-complete/auto-complete.component.ts
@@ -31,7 +31,7 @@ export class AutoCompleteComponent implements AfterViewChecked, ControlValueAcce
   @Input() public options:AutoCompleteOptions = new AutoCompleteOptions();
   @Input() public removeButtonClasses:string = '';
   @Input() public removeButtonColor:string = 'primary';
-  @Input() public removeButtonIcon:string = 'close';
+  @Input() public removeButtonIcon:string = 'close-circle';
   @Input() public removeButtonSlot:string = 'end';
   @Input() public removeDuplicateSuggestions:boolean = true;
   @Input() public showResultsFirst:boolean;
